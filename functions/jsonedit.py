52fce805a2a1dc03a1a38f7bf632c7faff2dd0aa
Fernando Garrido <siberiancoffe@outlook.cl>
2024-03-05 20:33:11 -0300

    configurations saved in "Documents"

--- functions/jsonedit.py
+++ functions/jsonedit.py
@@ -1,16 +1,38 @@
+from os import path
 import json
+from PySide6.QtCore import QStandardPaths, QDir
+
+DOCUMENT_PATH = QDir.fromNativeSeparators(
+    QStandardPaths.writableLocation(QStandardPaths.DocumentsLocation)
+)
+FILE_NAME = 'credentials_download_music_ytdlp.json'
+
 
 def saveValue(key: str, value: str) -> None:
-    with open('./credentials.json', 'r+') as r:
+    with open(f'{DOCUMENT_PATH}/{FILE_NAME}', 'r+') as r:
         data = json.load(r)
         data[key] = value
         r.seek(0)
         json.dump(data, r, indent=4)
         r.truncate()
 
+
 def getValue() -> tuple:
-    data = json.load(open('./credentials.json'))
+    if not path.isfile(f'{DOCUMENT_PATH}/{FILE_NAME}'):
+        createJsonConfig()
+
+    data = json.load(open(f'{DOCUMENT_PATH}/{FILE_NAME}'))
     client_id = data['client_id']
     client_secret = data['client_secret']
     dir = data['dir']
     return client_id, client_secret, dir
+
+
+def createJsonConfig() -> None:
+    with open(f'{DOCUMENT_PATH}/{FILE_NAME}', 'w') as r:
+        data = {
+            "client_id": "",
+            "client_secret": "",
+            "dir": ""
+        }
+        json.dump(data, r, indent=4)
